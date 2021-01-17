32e9214fddc05e562a5a0f24f967ec3de93a98eb
Natalia F <lu.jo159@gmail.com>
2021-01-17 18:58:02 +0300

    Add Count5PercDisc_TwoDiffBooksInBasket_ReturnCorrectSum() method in unit tests

--- HarryPotterKata.Tests/UnitTest1.cs
+++ HarryPotterKata.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using NUnit.Framework;
+using ThreeKatas;
 
 namespace HarryPotterKata.Tests
 {
@@ -7,8 +8,15 @@ namespace HarryPotterKata.Tests
     public class UnitTest1
     {
         [Test]
-        public void TestMethod1()
+        public void Count5PercDisc_TwoDiffBooksInBasket_ReturnCorrectSum()
         {
+            var basket = new Basket();
+            basket.Books.Add(new Book("Harry Potter and the Sorcerer’s Stone"));
+            basket.Books.Add(new Book("Harry Potter and the Chamber of Secrets"));
+
+            var result = basket.Count5PercentDiscount();
+
+            Assert.That(result, Is.EqualTo(15.2));
         }
     }
 }
---
+++ ThreeKatas/Basket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreeKatas
+{
+    public class Basket
+    {
+        public List<Book> Books { get; set; }
+        public Basket()
+        {
+            Books = new List<Book>();
+        }
+        public double Count5PercentDiscount()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
