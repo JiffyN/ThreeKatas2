bee60103f5cd4f1ca3790a9c3e3787fec6cf0646
Natalia F <lu.jo159@gmail.com>
2021-01-19 11:01:40 +0300

    Add unit test to print the same number

---
+++ FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace FizzBuzzKata.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void FizzBuzzMethod_NumberNotDivisibleBothByThreeOrFive_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.FizzBuzzMethod(4);
+
+            Assert.That(result, Is.EqualTo("4"));
+        }
+    }
+}
