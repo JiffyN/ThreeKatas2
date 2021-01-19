a6e65e8d5d80a99b1cf5e8a44cd3931baa1f6008
Natalia F <lu.jo159@gmail.com>
2021-01-19 11:11:02 +0300

    Fix method to return "Buzz"

--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -9,7 +9,10 @@
 
             if (number % 3 == 0)
                 return "Fizz";
-            
+
+            if (number % 5 == 0)
+                return "Buzz";
+
             return number.ToString();
         }
     }
