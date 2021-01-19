bc40b53c5f5ce8a86ff792b187d26abb3a546537
Natalia F <lu.jo159@gmail.com>
2021-01-19 18:59:06 +0300

    Add unit test to throw an exception if input is not an integer

--- FizzBuzzKata.Tests/FizzBuzzTests.cs
+++ FizzBuzzKata.Tests/FizzBuzzTests.cs
@@ -8,30 +8,37 @@ namespace FizzBuzzKata.Tests
         [Test]
         public void FizzBuzzMethod_NumberDivisibleByThreeAndFive_ReturnFizzBuzz()
         {
-            var result = FizzBuzz.FizzBuzzMethod(15);
+            var result = FizzBuzz.FizzBuzzMethod<int>(15);
 
             Assert.That(result, Is.EqualTo("FizzBuzz"));
         }
         [Test]
         public void FizzBuzzMethod_NumberDivisibleByThree_ReturnFizz()
         {
-            var result = FizzBuzz.FizzBuzzMethod(6);
+            var result = FizzBuzz.FizzBuzzMethod<int>(6);
 
             Assert.That(result, Is.EqualTo("Fizz"));
         }
         [Test]
         public void FizzBuzzMethod_NumberDivisibleByFive_ReturnBuzz()
         {
-            var result = FizzBuzz.FizzBuzzMethod(10);
+            var result = FizzBuzz.FizzBuzzMethod<int>(10);
 
             Assert.That(result, Is.EqualTo("Buzz"));
         }
         [Test]
         public void FizzBuzzMethod_NumberNotDivisibleBothByThreeOrFive_ReturnTheSameNumber()
         {
-            var result = FizzBuzz.FizzBuzzMethod(4);
+            var result = FizzBuzz.FizzBuzzMethod<int>(4);
 
             Assert.That(result, Is.EqualTo("4"));
+        }
+
+        [Test]
+        public void FizzBuzzMethod_NotIntValue_ThrowException()
+        {
+            Assert.That(() => FizzBuzz.FizzBuzzMethod<string>("f"), 
+                Throws.Exception.TypeOf<NonIntTypeException>());
         }
     }
 }
--- FizzBuzzKata/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz.cs
@@ -1,9 +1,17 @@
-﻿namespace FizzBuzzKata
+﻿using System;
+
+namespace FizzBuzzKata
 {
     public class FizzBuzz
     {
-        public static string FizzBuzzMethod(int number)
+        public static string FizzBuzzMethod<T>(T input)
         {
+            int number = 0;
+            if (typeof(T) != typeof(int))
+            {
+                throw new NonIntTypeException("Value can be only an integer");
+            }
+            number = Convert.ToInt32(input);
             if (number % 3 == 0 && number % 5 == 0)
                 return "FizzBuzz";
 
