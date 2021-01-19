b50cb648f37f2c3295c3414f321b4c3cd86e35cd
Natalia F <lu.jo159@gmail.com>
2021-01-19 21:41:54 +0300

    Add unit test to return "Prime"

---
+++ PrimeCompositeKata.Tests/PrimeCompositeTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace PrimeCompositeKata.Tests
+{
+    [TestFixture]
+    public class PrimeCompositeTests
+    {
+        [Test]
+        public void GetOutput_NumberIsPrime_ReturnPrime()
+        {
+            var result = PrimeComposite.GetOutput(5);
+
+            Assert.That(result, Is.EqualTo("Prime"));
+        }
+    }
+}
--- PrimeCompositeKata/PrimeComposite.cs
+++ PrimeCompositeKata/PrimeComposite.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
 namespace PrimeCompositeKata
 {
     public class PrimeComposite
     {
+        public static string GetOutput(int input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
